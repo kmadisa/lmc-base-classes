9f1fa76414714026e93b9c181b5bf4c53c9c79d0
Bruno Morgado <jb.morgado@gmail.com>
2019-08-08 12:30:57 +0100

    linting now runs in the all repository

--- code-analysis.sh
+++ code-analysis.sh
@@ -3,17 +3,6 @@ echo "STATIC CODE ANALYSIS"
 echo "===================="
 echo
 
-echo "MODULE ANALYSIS"
-echo "---------------"
-pylint --rcfile=.pylintrc skabase
-
-echo "TESTS ANALYSIS"
-echo "--------------"
-pylint --rcfile=.pylintrc skabase/SKAAlarmHandler/test
-pylint --rcfile=.pylintrc skabase/SKABaseDevice/test
-pylint --rcfile=.pylintrc skabase/SKACapability/test
-pylint --rcfile=.pylintrc skabase/SKALogger/test
-pylint --rcfile=.pylintrc skabase/SKAMaster/test
-pylint --rcfile=.pylintrc skabase/SKAObsDevice/test
-pylint --rcfile=.pylintrc skabase/SKASubarray/test
-pylint --rcfile=.pylintrc skabase/SKATelState/test
+# FIXME pylint needs to run twice since there is no way
+pylint -f colorized skabase
+pylint skabase > skabase_sca.xml
