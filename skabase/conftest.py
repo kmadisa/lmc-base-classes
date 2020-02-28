191d4dc448a8991a62d7c5db560a256054e54dec
Katleho Madisa <katleho.madisa47@gmail.com>
2020-02-28 05:57:12 +0000

    Reproduce error.

--- skabase/conftest.py
+++ skabase/conftest.py
@@ -55,7 +55,7 @@ def tango_context(request):
         tango_context = DeviceTestContext(klass)
 
     tango_context.start()
-    klass.get_name = mock.Mock(side_effect=tango_context.get_device_access)
+    #klass.get_name = mock.Mock(side_effect=tango_context.get_device_access)
 
     yield tango_context
     tango_context.stop()
