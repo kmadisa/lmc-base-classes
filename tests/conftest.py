07aede29aec8add28fd75b77b34fdf3ab0fe84c4
Drew Devereux <drew.devereux@csiro.au>
2020-07-06 14:18:21 +0800

    fixture docs update

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,36 +81,53 @@ def tango_change_event_helper(tango_context):
     Helper for testing tango change events. To use it, call the subscribe
     method with the name of the attribute for which you want change events.
     The returned value will be a callback handler that you can interrogate
-    with ``called``, ``expect_call_with``, ``expect_calls_with``,
-    ``value``, or ``values`` methods.  This interrogation must only be done
-    using a single call to **one** of those methods per expected event(s).::
+    with ``assert_not_called``, ``assert_call``, ``assert_calls``, and
+    ``value`` methods.::
 
     .. code-block:: python
 
         state_callback = tango_change_event_helper.subscribe("state")
-        assert state_callback.expect_call_with(DevState.OFF)
+        state_callback.assert_call(DevState.OFF)
 
         # Check that we can't turn off a device that isn't on
         with pytest.raises(DevFailed):
             tango_context.device.Off()
-        assert not state_callback.called()
+        state_callback.assert_not_called()
 
         # Now turn it on and check that we can turn it off
         tango_context.device.On()
-        assert state_callback.expect_call_with(DevState.ON)
+        state_callback.assert_call(DevState.ON)
 
         # Or we can test a sequence of events
         tango_context.device.Off()
         tango_context.device.On()
-        assert state_callback.expect_calls_with([DevState.OFF, DevState.ON])
+        state_callback.assert_calls([DevState.OFF, DevState.ON])
 
     """
     class _Callback:
         @staticmethod
         def subscribe(attribute_name):
+            """
+            Returns an event subscriber helper object that is subscribed
+            to change events on the named attribute.
+
+            :param attribute_name: name of the attribute for which
+                change events will be subscribed
+            :type attribute_name: str
+            :return: an event subscriber helper object
+            :rtype: object
+            """
             return _Callback(attribute_name)
 
         def __init__(self, attribute_name):
+            """
+            Creates an event subscriber helper object that is subscribed
+            to change events on the name attribute
+
+            :param attribute_name: name of the attribute for which
+                change events will be subscribed
+            :type attribute_name: str
+            """
             self._value = None
             self._values_queue = Queue()
             self._errors = []
@@ -123,10 +140,19 @@ def tango_change_event_helper(tango_context):
             )
 
         def __del__(self):
+            """
+            Unsubscribe from events before object is destroyed
+            """
             if hasattr(self, "_id"):
                 tango_context.device.unsubscribe_event(self._id)
 
         def __call__(self, event_data):
+            """
+            Event subscription callback
+
+            :param event_data: data about the change events
+            :type event_data: tango.EventData
+            """
             if event_data.err:
                 error = event_data.errors[0]
                 self._errors.append("Event callback error: [%s] %s" % (error.reason, error.desc))
@@ -134,6 +160,13 @@ def tango_change_event_helper(tango_context):
                 self._values_queue.put(event_data.attr_value.value)
 
         def _next(self):
+            """
+            Gets the attribute value from the next event if there is
+            one or if it arrives in time.
+
+            :return: an event, or None if there is no event
+            :rtype: tango.EventData
+            """
             assert not self._errors, f"Some errors: {self._errors}"
             try:
                 return self._values_queue.get(timeout=1.5)
@@ -142,6 +175,15 @@ def tango_change_event_helper(tango_context):
 
         @property
         def value(self):
+            """
+            The current value of the attribute according to received or
+            pending change events.
+
+            Note that reading this property consumes all pending events.
+
+            :return: the current value of the attribute according to
+            received or pending change events.
+            """
             new_value = self._next()
             while new_value is not None:
                 self._value = new_value
@@ -149,12 +191,41 @@ def tango_change_event_helper(tango_context):
             return self._value
 
         def assert_not_called(self):
+            """
+            Asserts that this callback has not been called with a
+            change event that has not already been consumed by ``value``,
+            ``assert_call`` or ``assert_calls`` methods.
+            """
             assert not self._values_queue.qsize()
 
         def assert_call(self, value):
+            """
+            Asserts that this callback has been called with a change
+            event that updates the attribute value to a given value.
+
+            Note that this method consumes a single event, but may leave
+            other events unconsumed.
+
+            :param value: the value that the attribute is asserted to
+                have been changed to
+            :type value: same as the attribute type
+            """
             assert self._next() == value
 
         def assert_calls(self, values):
+            """
+            Asserts that this callback has been called with a sequence
+            of change events that update the attribute values to the
+            given sequence of values.
+
+            Note that this method consumes the events associated with
+            the given values, but may leave other subsequence events
+            unconsumed.
+
+            :param values: sequence of values that the attribute
+                is asserted to have been changed to
+            :type values: list
+            """
             for value in values:
                 self.assert_call(value)
 
